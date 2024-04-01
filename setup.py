75ad17a6b42f3d765cbfba11936e966a053c2a9d
kunal <mehtakunalg@gmail.com>
2024-04-01 10:37:19 -0500

    setup and requirements updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="ner-using-bert",
+    version="0.0.1",
+    author="Kunal",
+    author_email="mehtakunalg@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
